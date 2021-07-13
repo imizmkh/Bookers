5562648443d070aa08ed953cc405147863219f6f
EC2 Default User <k.imaizumi1122@gmail.com>
2021-07-13 11:34:15 +0000

    [start]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,17 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @books = Book.find(params[:id])
   end
 
   def new
+    @books = Book.new
+  end
+
+  def create
   end
 
   def edit
